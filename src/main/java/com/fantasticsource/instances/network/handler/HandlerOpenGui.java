1c13edc3e694da569b4a37080d3965ee1b5277c8
Kyle Koder <silvertallon@gmail.com>
2019-07-03 07:16:30 -0700

    Fixed a dedicated mode startup issue

--- src/main/java/com/fantasticsource/instances/network/handler/HandlerOpenGui.java
+++ src/main/java/com/fantasticsource/instances/network/handler/HandlerOpenGui.java
@@ -9,10 +9,10 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public class HandlerOpenGui implements IMessageHandler<MessageOpenGui, IMessage>
 {
     @Override
+    @SideOnly(Side.CLIENT)
     public IMessage onMessage(MessageOpenGui message, MessageContext ctx)
     {
         Minecraft.getMinecraft().addScheduledTask(() -> Minecraft.getMinecraft().displayGuiScreen(new GuiCreateDimension()));
