5533643ef1ed66c92b2f65e8c9f6a09c8d0aa623
Kyle Koder <silvertallon@gmail.com>
2019-07-03 18:22:16 -0700

    Minor cleanup

--- src/main/java/com/fantasticsource/instances/server/commands/CommandTeleportD.java
+++ src/main/java/com/fantasticsource/instances/server/commands/CommandTeleportD.java
@@ -64,192 +64,189 @@ public class CommandTeleportD extends CommandBase
         {
             throw new WrongUsageException(Instances.MODID + ".commands.tpd.usage");
         }
+
+        byte b0 = 2;
+        Object object = null;
+
+        boolean dimensionThere = false;
+        int dimensionID = 0;
+        try
+        {
+            dimensionID = Integer.parseInt(args[0]);
+            dimensionThere = true;
+        }
+        catch (NumberFormatException exception)
+        {
+            for (Map.Entry<Integer, WorldInfoSimple> entry : InstanceHandler.instanceInfo.entrySet())
+            {
+                if (entry.getValue().getWorldName().equals(args[0]))
+                {
+                    dimensionID = entry.getKey();
+                    dimensionThere = true;
+                }
+            }
+        }
+
+        if (dimensionThere)
+        {
+            if (args.length == 1)
+            {
+                object = sender;
+                if (object instanceof EntityPlayerMP)
+                {
+                    EntityPlayerMP player = (EntityPlayerMP) object;
+
+                    if (player.dimension != dimensionID)
+                    {
+                        teleportEntityToDimension(server, player, dimensionID);
+                    }
+
+                    return;
+                }
+            }
+
+            if (args.length == 5 || args.length == 7)
+            {
+                object = getEntity(server, sender, args[1]);
+                b0 = 2;
+            }
+            else
+            {
+                object = getCommandSenderAsPlayer(sender);
+                b0 = 1;
+            }
+        }
         else
         {
-            byte b0 = 2;
-            Object object = null;
-
-            boolean dimensionThere = false;
-            int dimensionID = 0;
-            try
+            if (args.length == 1)
             {
-                dimensionID = Integer.parseInt(args[0]);
-                dimensionThere = true;
+                object = getCommandSenderAsPlayer(sender);
             }
-            catch (NumberFormatException exception)
+            else if (args.length == 2)
             {
-                for (Map.Entry<Integer, WorldInfoSimple> entry : InstanceHandler.instanceInfo.entrySet())
-                {
-                    if (entry.getValue().getWorldName().equals(args[0]))
-                    {
-                        dimensionID = entry.getKey();
-                        dimensionThere = true;
-                    }
-                }
+                object = getEntity(server, sender, args[0]);
             }
+        }
 
-            if (dimensionThere)
+        if (args.length != 1 && args.length != 2)
+        {
+            if (args.length < b0 + 3 || !dimensionThere)
             {
-                if (args.length == 1)
-                {
-                    object = sender;
-                    if (object instanceof EntityPlayerMP)
-                    {
-                        EntityPlayerMP player = (EntityPlayerMP) object;
-
-                        if (player.dimension != dimensionID)
-                        {
-                            teleportEntityToDimension(server, player, dimensionID);
-                        }
-
-                        return;
-                    }
-                }
-
-                if (args.length == 5 || args.length == 7)
-                {
-                    object = getEntity(server, sender, args[1]);
-                    b0 = 2;
-                }
-                else
-                {
-                    object = getCommandSenderAsPlayer(sender);
-                    b0 = 1;
-                }
+                throw new WrongUsageException(Instances.MODID + ".commands.tpd.usage");
             }
-            else
+            else if (((Entity) object).world != null)
             {
-                if (args.length == 1)
+                int i = b0 + 1;
+                CommandBase.CoordinateArg coordinatearg = parseCoordinate(((Entity) object).posX, args[b0], true);
+                CommandBase.CoordinateArg coordinatearg1 = parseCoordinate(((Entity) object).posY, args[i++], 0, 0, false);
+                CommandBase.CoordinateArg coordinatearg2 = parseCoordinate(((Entity) object).posZ, args[i++], true);
+                CommandBase.CoordinateArg coordinatearg3 = parseCoordinate((double) ((Entity) object).rotationYaw, args.length > i ? args[i++] : "~", false);
+                CommandBase.CoordinateArg coordinatearg4 = parseCoordinate((double) ((Entity) object).rotationPitch, args.length > i ? args[i] : "~", false);
+                float f;
+
+                if (((Entity) object).dimension != dimensionID)
                 {
-                    object = getCommandSenderAsPlayer(sender);
+                    object = teleportEntityToDimension(server, (Entity) object, dimensionID);
                 }
-                else if (args.length == 2)
-                {
-                    object = getEntity(server, sender, args[0]);
-                }
-            }
-
-            if (args.length != 1 && args.length != 2)
-            {
-                if (args.length < b0 + 3 || !dimensionThere)
-                {
-                    throw new WrongUsageException(Instances.MODID + ".commands.tpd.usage");
-                }
-                else if (((Entity) object).world != null)
-                {
-                    int i = b0 + 1;
-                    CommandBase.CoordinateArg coordinatearg = parseCoordinate(((Entity) object).posX, args[b0], true);
-                    CommandBase.CoordinateArg coordinatearg1 = parseCoordinate(((Entity) object).posY, args[i++], 0, 0, false);
-                    CommandBase.CoordinateArg coordinatearg2 = parseCoordinate(((Entity) object).posZ, args[i++], true);
-                    CommandBase.CoordinateArg coordinatearg3 = parseCoordinate((double) ((Entity) object).rotationYaw, args.length > i ? args[i++] : "~", false);
-                    CommandBase.CoordinateArg coordinatearg4 = parseCoordinate((double) ((Entity) object).rotationPitch, args.length > i ? args[i] : "~", false);
-                    float f;
-
-                    if (((Entity) object).dimension != dimensionID)
-                    {
-                        object = teleportEntityToDimension(server, (Entity) object, dimensionID);
-                    }
-
-                    if (object instanceof EntityPlayerMP)
-                    {
-                        EnumSet enumset = EnumSet.noneOf(SPacketPlayerPosLook.EnumFlags.class);
-
-                        if (coordinatearg.isRelative())
-                        {
-                            enumset.add(SPacketPlayerPosLook.EnumFlags.X);
-                        }
-
-                        if (coordinatearg1.isRelative())
-                        {
-                            enumset.add(SPacketPlayerPosLook.EnumFlags.Y);
-                        }
-
-                        if (coordinatearg2.isRelative())
-                        {
-                            enumset.add(SPacketPlayerPosLook.EnumFlags.Z);
-                        }
-
-                        if (coordinatearg4.isRelative())
-                        {
-                            enumset.add(SPacketPlayerPosLook.EnumFlags.X_ROT);
-                        }
-
-                        if (coordinatearg3.isRelative())
-                        {
-                            enumset.add(SPacketPlayerPosLook.EnumFlags.Y_ROT);
-                        }
-
-                        f = (float) coordinatearg3.getAmount();
-
-                        if (!coordinatearg3.isRelative())
-                        {
-                            f = MathHelper.wrapDegrees(f);
-                        }
-
-                        float f1 = (float) coordinatearg4.getAmount();
-
-                        if (!coordinatearg4.isRelative())
-                        {
-                            f1 = MathHelper.wrapDegrees(f1);
-                        }
-
-                        if (f1 > 90 || f1 < -90)
-                        {
-                            f1 = MathHelper.wrapDegrees(180 - f1);
-                            f = MathHelper.wrapDegrees(f + 180);
-                        }
-
-                        ((Entity) object).dismountRidingEntity();
-                        ((EntityPlayerMP) object).connection.setPlayerLocation(coordinatearg.getAmount(), coordinatearg1.getAmount(), coordinatearg2.getAmount(), f, f1, enumset);
-                        ((Entity) object).setRotationYawHead(f);
-                    }
-                    else
-                    {
-                        float f2 = (float) MathHelper.wrapDegrees(coordinatearg3.getResult());
-                        f = (float) MathHelper.wrapDegrees(coordinatearg4.getResult());
-
-                        if (f > 90 || f < -90)
-                        {
-                            f = MathHelper.wrapDegrees(180 - f);
-                            f2 = MathHelper.wrapDegrees(f2 + 180);
-                        }
-
-                        ((Entity) object).setLocationAndAngles(coordinatearg.getResult(), coordinatearg1.getResult(), coordinatearg2.getResult(), f2, f);
-                        ((Entity) object).setRotationYawHead(f2);
-
-                        ((Entity) object).world.updateEntityWithOptionalForce((Entity) object, false);
-                    }
-
-                    notifyCommandListener(sender, this, "commands.tp.success.coordinates", ((Entity) object).getName(), coordinatearg.getResult(), coordinatearg1.getResult(), coordinatearg2.getResult());
-                }
-            }
-            else
-            {
-                Entity entity = getEntity(server, sender, args[args.length - 1]);
-
-                if (entity.world != ((Entity) object).world)
-                {
-                    Entity toTeleport = (Entity) object;
-                    if (entity.dimension != toTeleport.dimension)
-                    {
-                        object = teleportEntityToDimension(server, toTeleport, entity.dimension);
-                    }
-                }
-
-                ((Entity) object).dismountRidingEntity();
-                ;
 
                 if (object instanceof EntityPlayerMP)
                 {
-                    ((EntityPlayerMP) object).connection.setPlayerLocation(entity.posX, entity.posY, entity.posZ, entity.rotationYaw, entity.rotationPitch);
+                    EnumSet enumset = EnumSet.noneOf(SPacketPlayerPosLook.EnumFlags.class);
+
+                    if (coordinatearg.isRelative())
+                    {
+                        enumset.add(SPacketPlayerPosLook.EnumFlags.X);
+                    }
+
+                    if (coordinatearg1.isRelative())
+                    {
+                        enumset.add(SPacketPlayerPosLook.EnumFlags.Y);
+                    }
+
+                    if (coordinatearg2.isRelative())
+                    {
+                        enumset.add(SPacketPlayerPosLook.EnumFlags.Z);
+                    }
+
+                    if (coordinatearg4.isRelative())
+                    {
+                        enumset.add(SPacketPlayerPosLook.EnumFlags.X_ROT);
+                    }
+
+                    if (coordinatearg3.isRelative())
+                    {
+                        enumset.add(SPacketPlayerPosLook.EnumFlags.Y_ROT);
+                    }
+
+                    f = (float) coordinatearg3.getAmount();
+
+                    if (!coordinatearg3.isRelative())
+                    {
+                        f = MathHelper.wrapDegrees(f);
+                    }
+
+                    float f1 = (float) coordinatearg4.getAmount();
+
+                    if (!coordinatearg4.isRelative())
+                    {
+                        f1 = MathHelper.wrapDegrees(f1);
+                    }
+
+                    if (f1 > 90 || f1 < -90)
+                    {
+                        f1 = MathHelper.wrapDegrees(180 - f1);
+                        f = MathHelper.wrapDegrees(f + 180);
+                    }
+
+                    ((Entity) object).dismountRidingEntity();
+                    ((EntityPlayerMP) object).connection.setPlayerLocation(coordinatearg.getAmount(), coordinatearg1.getAmount(), coordinatearg2.getAmount(), f, f1, enumset);
+                    ((Entity) object).setRotationYawHead(f);
                 }
                 else
                 {
-                    ((Entity) object).setLocationAndAngles(entity.posX, entity.posY, entity.posZ, entity.rotationYaw, entity.rotationPitch);
+                    float f2 = (float) MathHelper.wrapDegrees(coordinatearg3.getResult());
+                    f = (float) MathHelper.wrapDegrees(coordinatearg4.getResult());
+
+                    if (f > 90 || f < -90)
+                    {
+                        f = MathHelper.wrapDegrees(180 - f);
+                        f2 = MathHelper.wrapDegrees(f2 + 180);
+                    }
+
+                    ((Entity) object).setLocationAndAngles(coordinatearg.getResult(), coordinatearg1.getResult(), coordinatearg2.getResult(), f2, f);
+                    ((Entity) object).setRotationYawHead(f2);
+
+                    ((Entity) object).world.updateEntityWithOptionalForce((Entity) object, false);
                 }
 
-                notifyCommandListener(sender, this, "commands.tp.success", ((Entity) object).getName(), entity.getName());
+                notifyCommandListener(sender, this, "commands.tp.success.coordinates", ((Entity) object).getName(), coordinatearg.getResult(), coordinatearg1.getResult(), coordinatearg2.getResult());
             }
+        }
+        else
+        {
+            Entity entity = getEntity(server, sender, args[args.length - 1]);
+
+            if (entity.world != ((Entity) object).world)
+            {
+                Entity toTeleport = (Entity) object;
+                if (entity.dimension != toTeleport.dimension)
+                {
+                    object = teleportEntityToDimension(server, toTeleport, entity.dimension);
+                }
+            }
+
+            ((Entity) object).dismountRidingEntity();
+
+            if (object instanceof EntityPlayerMP)
+            {
+                ((EntityPlayerMP) object).connection.setPlayerLocation(entity.posX, entity.posY, entity.posZ, entity.rotationYaw, entity.rotationPitch);
+            }
+            else
+            {
+                ((Entity) object).setLocationAndAngles(entity.posX, entity.posY, entity.posZ, entity.rotationYaw, entity.rotationPitch);
+            }
+
+            notifyCommandListener(sender, this, "commands.tp.success", ((Entity) object).getName(), entity.getName());
         }
     }
 
