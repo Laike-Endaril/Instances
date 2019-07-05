28f99b2651363930de8c6190b6a7d846a7523e46
Kyle Koder <silvertallon@gmail.com>
2019-07-05 07:52:33 -0700

    Fixed improper super handling (not my fault)

--- src/main/java/com/fantasticsource/instances/world/WorldInfoSimple.java
+++ src/main/java/com/fantasticsource/instances/world/WorldInfoSimple.java
@@ -2,18 +2,15 @@ package com.fantasticsource.instances.world;
 
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.world.*;
+import net.minecraft.world.DimensionType;
+import net.minecraft.world.WorldSettings;
 import net.minecraft.world.storage.WorldInfo;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.UUID;
 
 public class WorldInfoSimple extends WorldInfo
 {
-    private WorldInfo superInfo;
     private DimensionType dimensionType = DimensionType.OVERWORLD;
     private UUID owner = null;
 
@@ -38,8 +35,6 @@ public class WorldInfoSimple extends WorldInfo
         {
             //Just keep default value from above
         }
-
-        superInfo = FMLCommonHandler.instance().getMinecraftServerInstance().getEntityWorld().getWorldInfo();
     }
 
     public WorldInfoSimple(WorldSettings settings, String name, DimensionType dimType)
@@ -55,55 +50,6 @@ public class WorldInfoSimple extends WorldInfo
         superNbt.setString("dimType", dimensionType.getName());
         superNbt.setString("owner", owner == null ? "null" : owner.toString());
         return superNbt;
-    }
-
-    @Override
-    public NBTTagCompound getPlayerNBTTagCompound()
-    {
-        return superInfo.getPlayerNBTTagCompound();
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public long getLastTimePlayed()
-    {
-        return superInfo.getLastTimePlayed();
-    }
-
-    @Override
-    public GameType getGameType()
-    {
-        return superInfo.getGameType();
-    }
-
-    @Override
-    public boolean isHardcoreModeEnabled()
-    {
-        return superInfo.isHardcoreModeEnabled();
-    }
-
-    @Override
-    public boolean areCommandsAllowed()
-    {
-        return superInfo.areCommandsAllowed();
-    }
-
-    @Override
-    public GameRules getGameRulesInstance()
-    {
-        return superInfo.getGameRulesInstance();
-    }
-
-    @Override
-    public EnumDifficulty getDifficulty()
-    {
-        return superInfo.getDifficulty();
-    }
-
-    @Override
-    public boolean isDifficultyLocked()
-    {
-        return superInfo.isDifficultyLocked();
     }
 
     public DimensionType getDimensionType()
