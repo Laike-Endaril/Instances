6d6118a5b322162a414ed3a483daff86cdca76bd
Kyle Koder <silvertallon@gmail.com>
2019-07-07 15:50:58 -0700

    Revert "Indev build 1.12.2.000f"
    This reverts commit 11083041ed9f243b8df67603b67a334cc061fdf9.

--- src/main/java/com/fantasticsource/instances/Instances.java
+++ src/main/java/com/fantasticsource/instances/Instances.java
@@ -34,7 +34,7 @@ public class Instances
 {
     public static final String MODID = "instances";
     public static final String NAME = "Instances";
-    public static final String VERSION = "1.12.2.000f";
+    public static final String VERSION = "1.12.2.000e";
 
     public static Integer nextFreeDimID()
     {
