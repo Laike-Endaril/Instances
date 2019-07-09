af696ce49e9c3a42c4f95ef309076b779d4d0333
Kyle Koder <silvertallon@gmail.com>
2019-07-09 11:04:53 -0700

    Moved personal instances to their own subdirectory ("personal").  Does not include the Library of Worlds for now because I'm rethinking the library atm

--- src/main/java/com/fantasticsource/instances/world/dimensions/skyroom/WorldProviderSkyroom.java
+++ src/main/java/com/fantasticsource/instances/world/dimensions/skyroom/WorldProviderSkyroom.java
@@ -1,9 +1,13 @@
 package com.fantasticsource.instances.world.dimensions.skyroom;
 
+import com.fantasticsource.instances.world.InstanceHandler;
+import com.fantasticsource.instances.world.InstanceWorldInfo;
 import com.fantasticsource.instances.world.dimensions.InstanceTypes;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.gen.IChunkGenerator;
+
+import javax.annotation.Nullable;
 
 public class WorldProviderSkyroom extends WorldProvider
 {
@@ -17,5 +21,18 @@ public class WorldProviderSkyroom extends WorldProvider
     public IChunkGenerator createChunkGenerator()
     {
         return new ChunkGeneratorSkyroom(world);
+    }
+
+    @Nullable
+    @Override
+    public String getSaveFolder()
+    {
+        int dim = getDimension();
+
+        String name = "Unknown_" + getDimensionType().name();
+        InstanceWorldInfo info = InstanceHandler.get(dim);
+        if (info != null) name = info.getWorldName();
+
+        return "./personal/" + name + "_" + dim;
     }
 }
