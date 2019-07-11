44a4dc5b7ce1f3a261bb37c9ab5b57547005e04c
Kyle Koder <silvertallon@gmail.com>
2019-07-11 06:40:23 -0700

    Indev build 1.12.2.000h

--- src/main/java/com/fantasticsource/instances/Instances.java
+++ src/main/java/com/fantasticsource/instances/Instances.java
@@ -42,7 +42,7 @@ public class Instances
 {
     public static final String MODID = "instances";
     public static final String NAME = "Instances";
-    public static final String VERSION = "1.12.2.000g";
+    public static final String VERSION = "1.12.2.000h";
 
     public static Integer nextFreeDimID()
     {
