304b0a53315a453ed594a1e71c14b0100b9e5a35
Kyle Koder <silvertallon@gmail.com>
2019-07-11 21:06:37 -0700

    All portals in personal instances are now personal portals again

--- src/main/java/com/fantasticsource/instances/world/dimensions/skyroom/ChunkGeneratorSkyroom.java
+++ src/main/java/com/fantasticsource/instances/world/dimensions/skyroom/ChunkGeneratorSkyroom.java
@@ -19,8 +19,7 @@ import java.util.List;
 public class ChunkGeneratorSkyroom implements IChunkGenerator
 {
     private static final IBlockState
-            PERSONAL_PORTAL = BlocksAndItems.blockPersonalPortal.getDefaultState(),
-            RETURN_PORTAL = BlocksAndItems.blockReturnPortal.getDefaultState(),
+            PORTAL = BlocksAndItems.blockPersonalPortal.getDefaultState(),
             BARRIER = Blocks.BARRIER.getDefaultState(),
             BEDROCK = Blocks.BEDROCK.getDefaultState(),
             FLOOR = Blocks.GRASS.getDefaultState();
@@ -111,15 +110,15 @@ public class ChunkGeneratorSkyroom implements IChunkGenerator
             {
                 if (chunkX == -1)
                 {
-                    chunk.setBlockState(new BlockPos(15, bottom + 2, 1), PERSONAL_PORTAL);
-                    chunk.setBlockState(new BlockPos(15, bottom + 3, 1), PERSONAL_PORTAL);
-                    chunk.setBlockState(new BlockPos(15, bottom + 4, 1), PERSONAL_PORTAL);
+                    chunk.setBlockState(new BlockPos(15, bottom + 2, 1), PORTAL);
+                    chunk.setBlockState(new BlockPos(15, bottom + 3, 1), PORTAL);
+                    chunk.setBlockState(new BlockPos(15, bottom + 4, 1), PORTAL);
                 }
                 else
                 {
-                    chunk.setBlockState(new BlockPos(0, bottom + 2, 1), RETURN_PORTAL);
-                    chunk.setBlockState(new BlockPos(0, bottom + 3, 1), RETURN_PORTAL);
-                    chunk.setBlockState(new BlockPos(0, bottom + 4, 1), RETURN_PORTAL);
+                    chunk.setBlockState(new BlockPos(0, bottom + 2, 1), PORTAL);
+                    chunk.setBlockState(new BlockPos(0, bottom + 3, 1), PORTAL);
+                    chunk.setBlockState(new BlockPos(0, bottom + 4, 1), PORTAL);
                 }
             }
         }
