26118b224de9158b863e74e0ad30fe8bdc349157
Kyle Koder <silvertallon@gmail.com>
2019-07-14 19:57:54 -0700

    Indev build 1.12.2.000o

--- src/main/java/com/fantasticsource/instances/Instances.java
+++ src/main/java/com/fantasticsource/instances/Instances.java
@@ -38,12 +38,12 @@ import net.minecraftforge.fml.relauncher.Side;
 
 import java.util.UUID;
 
-@Mod(modid = Instances.MODID, name = Instances.NAME, version = Instances.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.021,)")
+@Mod(modid = Instances.MODID, name = Instances.NAME, version = Instances.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.021,)", acceptableRemoteVersions = "[1.12.2.000n,1.12.2.000o]")
 public class Instances
 {
     public static final String MODID = "instances";
     public static final String NAME = "Instances";
-    public static final String VERSION = "1.12.2.000n";
+    public static final String VERSION = "1.12.2.000o";
 
     public static Integer nextFreeDimID()
     {
