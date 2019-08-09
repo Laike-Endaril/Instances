648610ba4ee79846ed7feb4ea443c508648fec18
Kyle Koder <silvertallon@gmail.com>
2019-08-08 20:36:03 -0700

    1.12.2.000q

--- src/main/java/com/fantasticsource/instances/Instances.java
+++ src/main/java/com/fantasticsource/instances/Instances.java
@@ -35,12 +35,12 @@ import net.minecraftforge.fml.relauncher.Side;
 
 import java.util.UUID;
 
-@Mod(modid = Instances.MODID, name = Instances.NAME, version = Instances.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.021,)")
+@Mod(modid = Instances.MODID, name = Instances.NAME, version = Instances.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.021,)", acceptableRemoteVersions = "[1.12.2.000p,1.12.2.000q]")
 public class Instances
 {
     public static final String MODID = "instances";
     public static final String NAME = "Instances";
-    public static final String VERSION = "1.12.2.000p";
+    public static final String VERSION = "1.12.2.000q";
 
     public static Integer nextFreeDimID()
     {
