1dea705169e2ff8b51ba80bc1f1b6cf0d7d824fc
Kyle Koder <silvertallon@gmail.com>
2021-01-18 01:04:20 -0800

    1.12.2.001g

--- src/main/java/com/fantasticsource/instances/Instances.java
+++ src/main/java/com/fantasticsource/instances/Instances.java
@@ -48,7 +48,7 @@ public class Instances
 {
     public static final String MODID = "instances";
     public static final String NAME = "Instances";
-    public static final String VERSION = "1.12.2.001f";
+    public static final String VERSION = "1.12.2.001g";
 
     public static Integer nextFreeDimID()
     {
